0c9a9618e941ab7adc5d4e1d9ef07a979f604280
jainks-1 <jainks@mail.uc.edu>
2025-02-16 16:19:46 -0500

    Code complete

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,49 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// Kaden Jain
+// Computer Programming 1 - Spring 2025
+/* (This task uses Strings:) A program that prompts
+the user for their party affiliation (Democrat, Republican,
+or Independent) they respond with the first letter, and the
+program responds accordingly with a Donkey, Elephant, Person,
+or Other. (i.e. "You get a Democratic Donkey.")
+
+Notes: create a menu so the user chooses D, R, or I and assume
+that any other choice will be Other.
+ */
+
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // declare variables
+        Scanner in = new Scanner(System.in);
+        String userParty = "";
+
+        // get input
+        System.out.println("D-Democrat\t R-Republican\t I-Independent\t \nPlease Enter your party affiliation: ");
+        // in.hasNext();
+        userParty = in.next();
+        in.nextLine();
+
+            // determine party affiliation if/else
+            if (userParty.equalsIgnoreCase("D")){
+
+                System.out.println("You get a Democratic donkey!");
+
+            }
+            else if (userParty.equalsIgnoreCase("R")) {
+
+                System.out.println("You get a Republican elephant!");
+
+            }
+            else if (userParty.equalsIgnoreCase("I")) {
+
+                System.out.println("You are an Independent person!");
+
+            }
+            else {
+
+                System.out.println("You chose an 'other' party. You get a thumbs up.");
+
+            }
     }
 }
